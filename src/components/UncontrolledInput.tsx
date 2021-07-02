ca4530bc4cb70967d0b265b3158f342cbdba6a0d
Co9xs <ryo.fjsm0203@icloud.com>
2021-07-02 18:04:30 +0900

    fix message

--- src/components/UncontrolledInput.tsx
+++ src/components/UncontrolledInput.tsx
@@ -6,7 +6,7 @@ type Props = JSX.IntrinsicElements['input'] & {}
 export const UncontrolledInput: React.VFC<Props> = () => {
   const ref = useRef<HTMLInputElement>()
   const handleBlur = e => {
-    console.log({"value": ref.current.value})
+   console.log({value: ref.current.value})
   }
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -10,22 +10,22 @@ class App extends React.Component {
         <div>
           <div style={{background: '#EFEFEF', marginBottom: '30px', padding: '10px'}}>
             <h2 style={{margin: "0"}}>Controlled</h2>
-            <p style={{margin: "0"}}>状態をReactで完全に制御してonChangeでstateを更新してvalueに反映</p>
+            <p style={{margin: "0"}}>状態をuseStateで管理しつつ、onChangeでstateを更新してvalueに反映</p>
             <p style={{margin: "0"}}>入力中のinputの状態に応じて他の部分をインタラクティブに変更したい時に便利（リアルタイムなバリデーション, etc）</p>
             <ControlledInput/>
           </div>
 
           <div style={{background: '#EFEFEF', marginBottom: '30px', padding: '10px'}}>
             <h2 style={{margin: "0"}}>Uncontrolled</h2>
-            <p style={{margin: "0"}}>状態をDOMに持たせたまま、任意のタイミングでrefから値を取り出す（今回はonBlurでconsoleに表示している）</p>
+            <p style={{margin: "0"}}>useStateは使わず状態はDOMに持たせたまま、任意のタイミングでrefから値を取り出す（今回はonBlurでconsoleに表示している）</p>
             <p style={{margin: "0"}}>DOMに値を保存しているので、React以外のJSコードからでも参照しやすい</p>
             <UncontrolledInput/>
           </div>
 
           <div style={{background: '#EFEFEF', marginBottom: '30px', padding: '10px'}}>
             <h2 style={{margin: "0"}}>Mixed</h2>
-            <p style={{margin: "0"}}>状態はDOMに持たせつつ、onBlurでstateに同期</p>
-            <p style={{margin: "0"}}>再レンダリングが走る回数が少ない分パフォーマンス◯</p>
+            <p style={{margin: "0"}}>入力中の状態はDOMに持たせつつ、任意のタイミングでstateに同期（今回はonBlurでstateに同期している）</p>
+            <p style={{margin: "0"}}>状態はReactで保持しつつ、再レンダリングが走る回数が少ない分パフォーマンス◯</p>
             <MixedInput/>
           </div>
 
